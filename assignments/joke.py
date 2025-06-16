0a2ba8278655e522a03bea09f2c96f1b346b93eb
CrvptiK <cil-vivien.klein@stud.leuphana.de>
2025-06-16 12:32:42 +0200

    add joke

---
+++ assignments/joke.py
@@ -0,0 +1,9 @@
+import cowsay
+import pyjokes
+
+joke = pyjokes.get_joke(language='en')
+
+print(cowsay.dragon(joke))
+
+
+#did i seriously almost forget uploading this? maybe. barely on time i hope
